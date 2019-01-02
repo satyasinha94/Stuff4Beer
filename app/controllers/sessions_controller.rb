9cf47088cf8009dc537fb6a4fed8dfeecc68db2b
satyasinha94 <satyasinha94@gmail.com>
2019-01-02 16:44:57 -0500

    session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  # before_action :authorized comes from ApplicationController
+
+  # skip_before_action :authorized, only: [:new, :create]
+
+  def new
+    render :new
+  end
+
+  def create #handles the POST request to /login
+    byebug
+    @user = User.find_by(name: params[:name])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:notice] = 'Invalid username or password'
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    # session[:user_id] = nil
+    flash[:notice] = 'u logged out'
+    redirect_to login_path
+  end
+
+end
