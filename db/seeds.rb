6390dafa08bc8322f081d30b017f4f2faffd8112
satyasinha94 <satyasinha94@gmail.com>
2019-01-03 12:07:23 -0500

    listing styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,21 @@
 
 
 
-Category.create(name: "Beer")
-Category.create(name: "Wine")
-Category.create(name: "Whiskey")
-Category.create(name: "Rum")
-Category.create(name: "Tequila")
-Category.create(name: "Gin")
+# Category.create(name: "Beer")
+# Category.create(name: "Wine")
+# Category.create(name: "Whiskey")
+# Category.create(name: "Rum")
+# Category.create(name: "Tequila")
+# Category.create(name: "Gin")
+#
+# User.create(name: "Satya", email: "ssinha@example.com", password: "1234")
+25.times do
+  User.create(name: Faker::Name.unique.name, email: Faker::Internet.email, password: Faker::Internet.password)
+end
 
-User.create(name: "Satya", email: "ssinha@example.com", password: "1234")
-
-Listing.create(title: "l2", body: "4gh43h", user_id: 1, category_id: 1)
-Listing.create(title: "l3", body: "4gh4nhtn3h", user_id: 1, category_id: 2)
-Listing.create(title: "l4", body: "4grwegwh43h", user_id: 1, category_id: 3)
+# Listing.create(title: "l2", body: "4gh43h", user_id: 1, category_id: 1)
+# Listing.create(title: "l3", body: "4gh4nhtn3h", user_id: 1, category_id: 2)
+# Listing.create(title: "l4", body: "4grwegwh43h", user_id: 1, category_id: 3)
+100.times do
+  Listing.create(title: Faker::Beer.unique.name, body: Faker::Lorem.sentence, user_id: rand(User.first.id..User.last.id), category_id: 1)
+end
